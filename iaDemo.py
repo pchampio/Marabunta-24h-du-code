a15e0da7e4143d5aa27864ade0ac8bf801558adf
Valentin Pelloin <valentinpelloin@gmail.com>
2018-01-20 17:25:46 +0100

    update demo

--- iaDemo.py
+++ iaDemo.py
@@ -1,24 +1,27 @@
 from wrapper import Protocol
 
-t, obj = Protocol.readInput()
-
-if t == 'ANT':
-	ant = obj
+def antIa(ant):
+	# ANT PROGRAM
 	ant.explorer()
-	Protocol.exit()
 
-elif t == 'NEST':
-	nest = obj
+def nestIA(ant):
+	# NEST PROGRAM
 
 	if nest.memory[0] == 0:
 		nest.setMemoryLocation(0, 1)
 		nest.memory()
 		nest.newAnt(0)
+
 	elif nest.memory[0] == 1:
 		nest.setMemoryLocation(0, 2)
 		nest.memory()
 		nest.antOut(0, 50, 0, 0)
 
-	Protocol.exit()
 
-Protocol.exit()
+while True:
+	nameEntity, entity = Protocol.readInput()
+	if nameEntity == 'ANT':
+		antIA(entity)
+	elif nameEntity == 'NEST':
+		nestIA(entity)
+	Protocol.exit()
