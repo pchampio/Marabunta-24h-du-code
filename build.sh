ef2b5b04a47c43569671183b45011b6b5dd79d57
Valentin Pelloin <valentinpelloin@gmail.com>
2018-01-21 00:54:04 +0100

    compile python 😇

--- build.sh
+++ build.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+# Of course, we need to compile python!
+
+i=1
+sp="⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"
+echo -n ' '
+for t in {1..30}
+do
+	printf "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b ${sp:i++%${#sp}:1} Compiling python..."
+	sleep 0.1
+done
+
+echo "Done !"
+
