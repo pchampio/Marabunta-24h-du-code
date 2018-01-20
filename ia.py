a6464efefc749e46e3194092cf59d0dce54adea7
Valentin Pelloin <valentinpelloin@gmail.com>
2018-01-20 16:20:00 +0100

    ia

--- ia.py
+++ ia.py
@@ -1,12 +1,26 @@
-import wrapper.wrapper import Protocol
+import wrapper.wrapper 
+import Protocol
 
 t, obj = Protocol.readInput()
 
 
-if t == 'ANT'
+if t == 'ANT':
 	ant = obj
-	# action de ant
 
-elif t == 'NEST'
+	# action de ant
+	if ant.type == AntType.exploratrice:
+		idPath  = ant.m1
+		gotFood = ant.m2 
+
+		if ant.stamina < MIN_STAMINA_BEFORE_EAT:
+			ant.eat(1)
+			Protocol.exit()
+		
+		ph = ant.arrSeePheromone
+		
+	elif ant.type == AntType.ramasseuse:
+		Protocol.exit()
+
+elif t == 'NEST':
 	nest = obj
 	# action de nest
